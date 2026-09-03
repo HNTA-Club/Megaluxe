6b592af7161a8d35e2b8f127e729dc4049b7a750
Ari Hietanen <ari.hietanen@estiem.org>
2026-09-03 03:04:16 +0300

    feat(highscore): redesign dashboard with container cards, key metrics, and dynamic highlight pool

--- generate_xml.py
+++ generate_xml.py
@@ -17,6 +17,19 @@ def create_text(name, x, y, h, align='Left', text='', max_w='0', style='Bold', c
       <Text>{text}</Text>
     </Text>'''
 
+def create_static(name, x, y, w, h, color='BackgroundColorAlpha'):
+    return f'''    <Static Name="{name}">
+      <Skin />
+      <Color Name="{color}" />
+      <Rect>
+        <X>{x}</X>
+        <Y>{y}</Y>
+        <W>{w}</W>
+        <H>{h}</H>
+        <Z>0</Z>
+      </Rect>
+    </Static>'''
+
 def create_particle(name, x, y, w, h):
     return f'''    <ParticleEffect Name="{name}">
       <Skin>Star</Skin>
@@ -33,46 +46,60 @@ def create_particle(name, x, y, w, h):
       <MaxNumber>100</MaxNumber>
     </ParticleEffect>'''
 
+statics = []
+statics.append(create_static('StaticMenuBar', 0, 60, 1920, 67.5))
+statics.append(create_static('StaticCardCurrent', 60, 140, 870, 450))
+statics.append(create_static('StaticCardSeason', 990, 140, 870, 450))
+statics.append(create_static('StaticCardLore', 60, 620, 870, 410))
+statics.append(create_static('StaticCardHighlight', 990, 620, 870, 410))
+
 texts = []
 
-# Headers
+# Top Menu Header
 texts.append(create_text('TextTitle', 90, 75, 40.5, 'Left', 'TR_SCREENHIGHSCORE_HIGHSCORE'))
 texts.append(create_text('TextSongName', 960, 75, 40.5, 'Center'))
 texts.append(create_text('TextSongMode', 1830, 75, 40.5, 'Right'))
 
-# Quadrant 1: CURRENT PERFORMANCE (MASSIVE)
-texts.append(create_text('TextCurrentTitle', 120, 140, 45, 'Left', color='SelectedColor'))
-for i in range(1, 5):
-    y = 250 + (i-1)*80
-    texts.append(create_text(f'TextCurrentName{i}', 150, y, 75, 'Left', max_w='500'))
-    texts.append(create_text(f'TextCurrentScore{i}', 850, y, 75, 'Right'))
-    texts.append(create_text(f'TextCurrentRecord{i}', 870, y, 40, 'Left', color='SelectedColor'))
+# Card 1 (Top-Left): Current Performance / All-Time Top (Up to 6 rows)
+texts.append(create_text('TextCurrentTitle', 90, 160, 38, 'Left', color='SelectedColor'))
+for i in range(1, 7):
+    y = 210 + (i-1)*52
+    texts.append(create_text(f'TextCurrentName{i}', 110, y, 48, 'Left', max_w='460'))
+    texts.append(create_text(f'TextCurrentScore{i}', 780, y, 48, 'Right'))
+    texts.append(create_text(f'TextCurrentRecord{i}', 800, y, 32, 'Left', color='SelectedColor'))
 
-# Quadrant 2: SEASON LEADERBOARD
-texts.append(create_text('TextSeasonTitle', 1020, 140, 40, 'Left', color='SelectedColor'))
-texts.append(create_text('TextSeasonSubTitle', 1020, 185, 26, 'Left'))
+# Card 2 (Top-Right): Season Leaderboard
+texts.append(create_text('TextSeasonTitle', 1020, 160, 38, 'Left', color='SelectedColor'))
+texts.append(create_text('TextSeasonSubTitle', 1830, 168, 26, 'Right'))
 for i in range(1, 6):
     y = 225 + (i-1)*65
-    texts.append(create_text(f'TextSeasonRank{i}', 1020, y, 38, 'Left'))
-    texts.append(create_text(f'TextSeasonScore{i}', 1180, y, 38, 'Right'))
-    texts.append(create_text(f'TextSeasonName{i}', 1200, y, 38, 'Left', max_w='300'))
-    texts.append(create_text(f'TextSeasonDiff{i}', 1520, y, 35, 'Left'))
-    texts.append(create_text(f'TextSeasonDate{i}', 1830, y, 35, 'Right'))
+    texts.append(create_text(f'TextSeasonRank{i}', 1020, y, 36, 'Left'))
+    texts.append(create_text(f'TextSeasonScore{i}', 1180, y, 36, 'Right'))
+    texts.append(create_text(f'TextSeasonName{i}', 1200, y, 36, 'Left', max_w='280'))
+    texts.append(create_text(f'TextSeasonDiff{i}', 1520, y, 32, 'Left'))
+    texts.append(create_text(f'TextSeasonDate{i}', 1830, y, 32, 'Right'))
 
-# Quadrant 3: SONG LORE
-texts.append(create_text('TextLoreTitle', 120, 650, 45, 'Left', color='SelectedColor'))
-for i in range(1, 5):
-    y = 720 + (i-1)*50
-    texts.append(create_text(f'TextLoreStat{i}', 150, y, 38, 'Left', max_w='750'))
+# Card 3 (Bottom-Left): Song Lore (Side-by-Side Key Metrics with Units)
+texts.append(create_text('TextLoreTitle', 90, 640, 38, 'Left', color='SelectedColor'))
 
-# Quadrant 4: CLUB HIGHLIGHTS
-texts.append(create_text('TextHighlightTitle', 1020, 650, 45, 'Left', color='SelectedColor'))
-texts.append(create_text('TextHighlightBody', 1050, 720, 50, 'Left', max_w='750'))
+# Left Column (Performances)
+texts.append(create_text('TextLoreStat1_Num', 110, 700, 70, 'Left', color='TextColor', max_w='380'))
+texts.append(create_text('TextLoreStat1', 110, 785, 28, 'Left', max_w='380'))
+texts.append(create_text('TextLoreStat4', 110, 825, 26, 'Left', max_w='380'))
+
+# Right Column (Record & Date)
+texts.append(create_text('TextLoreStat2_Num', 500, 700, 70, 'Left', color='TextColor', max_w='410'))
+texts.append(create_text('TextLoreStat2', 500, 785, 28, 'Left', max_w='410'))
+texts.append(create_text('TextLoreStat3', 500, 825, 26, 'Left', max_w='410'))
+
+# Card 4 (Bottom-Right): Club Highlight (Hero Metric Card)
+texts.append(create_text('TextHighlightTitle', 1020, 640, 38, 'Left', color='SelectedColor'))
+texts.append(create_text('TextHighlightBody', 1425, 750, 44, 'Center', max_w='800'))
 
 particles = []
-for i in range(1, 5):
-    y = 250 + (i-1)*80
-    particles.append(create_particle(f'ParticleEffectNew{i}', 150, y-10, 700, 90))
+for i in range(1, 7):
+    y = 210 + (i-1)*52
+    particles.append(create_particle(f'ParticleEffectNew{i}', 110, y-5, 700, 50))
 
 xml_content = f'''<?xml version='1.0' encoding='utf-8'?>
 <Screen xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
@@ -90,17 +117,7 @@ xml_content = f'''<?xml version='1.0' encoding='utf-8'?>
     </Background>
   </Backgrounds>
   <Statics>
-    <Static Name="StaticMenuBar">
-      <Skin />
-      <Color Name="BackgroundColorAlpha" />
-      <Rect>
-        <X>0</X>
-        <Y>60</Y>
-        <W>1920</W>
-        <H>67.5</H>
-        <Z>0</Z>
-      </Rect>
-    </Static>
+{chr(10).join(statics)}
   </Statics>
   <Texts>
 {chr(10).join(texts)}
